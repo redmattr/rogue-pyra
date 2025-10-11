b739406cf01a4e689c7039c80d4044cc3891f71a
redmattr <mjrst268@mail.rmu.edu>
2025-10-10 20:50:48 -0400

    Added necessary entity classes for the game

---
+++ RoguePyra/Entity/EntityPhysical.cs
@@ -0,0 +1,25 @@
+﻿using RoguePyra.Physics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoguePyra.Entity
+{
+    public class EntityPhysical : CollisionObj
+    {
+        public Vector2 Position { get; set; } //Getter|Setter for entity position
+        public Vector2 Velocity { get; set; } //Vector Velocity for approximate movement
+        public Vector2 Force { get; set; } //For physics calculations
+        public float mass { get; set; } //Mass of entity
+        public bool _IsGrav { get; set; } //Is entity affected by grav?
+
+        //Friction Coefficients
+        public float DynamicFriction { get; set; }
+        public float StaticFriction { get; set; }
+
+        public float Elasticity { get; set; } //Bounciness (when colliding)
+    }
+}
